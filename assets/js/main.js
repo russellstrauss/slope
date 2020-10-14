e808070775f9f91d43f973ffe852c8b9e0a89ff2
Russell Strauss <russellstrauss@gmail.com>
2020-10-14 01:02:27 -0400

    init commit

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+var Slope = require('./components/slope.js');
+var Utilities = require('./utils.js');
+var Graphics = require('./graphics.js');
+
+(function () {
+	
+	document.addEventListener('DOMContentLoaded',function(){
+
+		Slope().init();
+	});
+})();
